716e2df628d203cb253658a367a972a561d7995b
Developer123485 <Developer123485@example.com>
2025-08-03 13:06:07 +0530

    api changes

--- app/services/tracesActivities.service.js
+++ app/services/tracesActivities.service.js
@@ -3,7 +3,10 @@ import api from "../utils/interceptors";
 export const TracesActivitiesService = {
     autoFillLogin,
     startLogin,
-    continueRequestConsoFile
+    continueRequestConsoFile,
+    continueRequest27D,
+    continueRequest16,
+    continueRequest16A
 };
 
 async function autoFillLogin(model) {
@@ -20,4 +23,20 @@ async function continueRequestConsoFile(model) {
     const result = await api.post("tracesActivities/continueRequestConsoFile", model);
     return result;
 }
+
+async function continueRequest16(model) {
+    const result = await api.post("tracesActivities/continueRequest16", model);
+    return result;
+}
+
+async function continueRequest16A(model) {
+    const result = await api.post("tracesActivities/continueRequest16A", model);
+    return result;
+}
+
+async function continueRequest27D(model) {
+    const result = await api.post("tracesActivities/continueRequest27D", model);
+    return result;
+}
+
 
