d6e84357b99e563e875c7ea84215ff7d10184afd
Developer123485 <Developer123485@example.com>
2025-08-04 17:29:56 +0530

    api changes

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const baseUrl = "https://localhost:44319/api/";
+const baseUrl = "https://api.taxvahan.site/api/";
 export const apiRequest = axios.create({ baseURL: baseUrl });
-export const apiUrl = "https://localhost:44319/api/";
+export const apiUrl = "https://api.taxvahan.site/api/";
