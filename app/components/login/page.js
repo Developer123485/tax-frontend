f4cde6d982f2ab36913f0c5c419248fcc0e84f5d
bhojrajmukati <dev.rahulsharma80@gmail.com>
2025-09-05 17:12:42 +0530

    fixed form width

--- app/components/login/page.js
+++ app/components/login/page.js
@@ -117,155 +117,160 @@ export default function LoginForm() {
             <HomeBanner
               subheading="Welcome To"
               heading="TaxVahan"
-              paragraph="We're delighted to have you back! Access your account now by logging in to continue your journey with us."
+              paragraph={
+                <>
+                  We're delighted to have you back! Access your account <br />{" "}
+                  now by logging in to continue your journey with us.
+                </>
+              }
               backgroundImage="/images/signin_bg.jpg"
             ></HomeBanner>
             <div className="col-md-6 d-flex form-sec">
-              <div className="px-2 py-5 mx-md-4 px-md-4 py-md-4 bg-white content-box rounded-3">
-                <h4 className="mb-0 mb-md-3 text-capitalize">Login</h4>
-                <br />
-                <div className="row g-3">
-                  <form>
-                    <div className="col-md-12 mb-3">
-                      <label htmlFor="inputemail" className="form-label">
-                        <span>Email Address</span>
-                        <span className="text-danger"> *</span>
-                      </label>
-                      <div className="input-group mb-2">
-                        <span className="input-group-text pe-0">
-                          <span className="pe-2 border-end">
-                            <Image
-                              className="pe-1"
-                              src="/images/icons/form_mail_icon.svg"
-                              alt="form_mail_icon"
-                              width={24}
-                              height={25}
-                            />
-                          </span>
-                        </span>
-                        <input
-                          type="text"
-                          placeholder=""
-                          className="form-control"
-                          id="inputemail"
-                          autoComplete="off"
-                          value={loginDetails.email}
-                          onChange={(e) => {
-                            e.preventDefault();
-                            setLoginDetails((prevState) => ({
-                              ...prevState,
-                              email: e.target.value,
-                            }));
-                          }}
-                        />
-                      </div>
-                      <div className="">
-                         {isDirty && loginDetails.emailError && (
-                          <span className="text-danger">
-                            {loginDetails.emailError}
-                          </span>
-                        )}
-                      </div>
-                    </div>
-                    <div className="col-md-12 mb-3 password-container">
-                      <label htmlFor="inputPassword" className="form-label">
-                        <span>Password</span>
-                        <span className="text-danger"> *</span>
-                      </label>
-                      <div className="input-group mb-2">
-                        <span className="input-group-text pe-0">
-                          <span className="pe-2 border-end">
-                            <Image
-                              className="pe-1"
-                              src="/images/icons/form_pass_icon.svg"
-                              alt="form_pass_icon"
-                              width={24}
-                              height={25}
-                            />
-                          </span>
-                        </span>
-                        <input
-                          type={passwordVisible ? "text" : "password"}
-                          placeholder=""
-                          className="form-control"
-                          id="inputPassword"
-                          autoComplete="off"
-                          value={loginDetails.password}
-                          onChange={(e) => {
-                            e.preventDefault();
-                            setLoginDetails((prevState) => ({
-                              ...prevState,
-                              password: e.target.value,
-                            }));
-                          }}
-                        />
-                        <button
-                          type="button"
-                          onClick={togglePassword}
-                          className="eye-icon"
-                        >
-                          {passwordVisible ? <FaEye /> : <FaEyeSlash />}
-                        </button>
-                      </div>
-                      <div className="">
-                        {isDirty && loginDetails.passwordError && (
-                          <span className="text-danger">
-                            {loginDetails.passwordError}
-                          </span>
-                        )}
-                      </div>
-                      <div className="mb-2 d-flex justify-content-between align-items-center">
-                        <div className="form-check mb-0 d-flex align-items-center">
-                          <input
-                            className="form-check-input rem-me"
-                            type="checkbox"
-                            id="rememberMe"
-                          />
-                          <label
-                            className="form-check-label mt-1 opacity-75"
-                            for="rememberMe"
-                          >
-                            Remember me
+              <div className="px-2 py-5 mx-md-4 px-md-4 py-md-4 bg-white content-box border border-1 rounded-3">
+                <h4 className="mb-0 mb-md-4 text-capitalize">Login</h4>
+                <div>
+                  <div className="content">
+                    <form>
+                      <div className="row g-3">
+                        <div className="col-md-12">
+                          <label htmlFor="inputemail" className="form-label">
+                            <span>Email Address</span>
+                            <span className="text-danger"> *</span>
                           </label>
+                          <div className="input-group mb-2">
+                            <span className="input-group-text pe-0">
+                              <span className="pe-2 border-end">
+                                <Image
+                                  className="pe-1"
+                                  src="/images/icons/form_mail_icon.svg"
+                                  alt="form_mail_icon"
+                                  width={24}
+                                  height={25}
+                                />
+                              </span>
+                            </span>
+                            <input
+                              type="text"
+                              placeholder=""
+                              className="form-control"
+                              id="inputemail"
+                              autoComplete="off"
+                              value={loginDetails.email}
+                              onChange={(e) => {
+                                e.preventDefault();
+                                setLoginDetails((prevState) => ({
+                                  ...prevState,
+                                  email: e.target.value,
+                                }));
+                              }}
+                            />
+                          </div>
+                          <div className="">
+                            {isDirty && loginDetails.emailError && (
+                              <span className="text-danger">
+                                {loginDetails.emailError}
+                              </span>
+                            )}
+                          </div>
+                        </div>
+                        <div className="col-md-12 password-container">
+                          <label htmlFor="inputPassword" className="form-label">
+                            <span>Password</span>
+                            <span className="text-danger"> *</span>
+                          </label>
+                          <div className="input-group mb-2">
+                            <span className="input-group-text pe-0">
+                              <span className="pe-2 border-end">
+                                <Image
+                                  className="pe-1"
+                                  src="/images/icons/form_pass_icon.svg"
+                                  alt="form_pass_icon"
+                                  width={24}
+                                  height={25}
+                                />
+                              </span>
+                            </span>
+                            <input
+                              type={passwordVisible ? "text" : "password"}
+                              placeholder=""
+                              className="form-control"
+                              id="inputPassword"
+                              autoComplete="off"
+                              value={loginDetails.password}
+                              onChange={(e) => {
+                                e.preventDefault();
+                                setLoginDetails((prevState) => ({
+                                  ...prevState,
+                                  password: e.target.value,
+                                }));
+                              }}
+                            />
+                            <button
+                              type="button"
+                              onClick={togglePassword}
+                              className="eye-icon"
+                            >
+                              {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+                            </button>
+                          </div>
+                          <div className="">
+                            {isDirty && loginDetails.passwordError && (
+                              <span className="text-danger">
+                                {loginDetails.passwordError}
+                              </span>
+                            )}
+                          </div>
+                          <div className="mb-2 d-flex justify-content-between align-items-center">
+                            <div className="form-check mb-0 d-flex align-items-center">
+                              <input
+                                className="form-check-input rem-me"
+                                type="checkbox"
+                                id="rememberMe"
+                              />
+                              <label
+                                className="form-check-label mt-1 opacity-75"
+                                for="rememberMe"
+                              >
+                                Remember me
+                              </label>
+                            </div>
+
+                            <button
+                              type="button"
+                              className="btn btn-link py-0 mt-1 fs-12"
+                              onClick={(e) => {
+                                router.push("/forgot-password");
+                              }}
+                            >
+                              Forgot password?
+                            </button>
+                          </div>
                         </div>
 
-                        <button
-                          type="button"
-                          className="btn btn-link py-0 mt-1 fs-12"
-                          onClick={(e) => {
-                            router.push("/forgot-password");
-                          }}
-                        >
-                          Forgot password?
-                        </button>
-                      </div>
-                    </div>
-
-                    <div className="col-md-12">
-                      <button
-                        type="button"
-                        onClick={(e) => submitLoginDetail(e)}
-                        className="btn btn-pri-grd text-white w-100 position-relative"
-                      >
-                        {loading && (
-                          <span
-                            className="spinner-grow spinner-grow-sm"
-                            role="status"
-                            aria-hidden="true"
-                          ></span>
-                        )}
-                        Log In
-                        <Image
-                          className="position-absolute top-50 end-0 me-2 translate-middle"
-                          src="/images/icons/btn_arrow.svg"
-                          alt="btn_arrow"
-                          width={24}
-                          height={25}
-                        />
-                      </button>
-                    </div>
-                  </form>
-{/*                   <div className="text-center d-flex flex-md-row align-items-center justify-content-center">
+                        <div className="col-md-12">
+                          <button
+                            type="button"
+                            onClick={(e) => submitLoginDetail(e)}
+                            className="btn btn-pri-grd text-white w-100 position-relative"
+                          >
+                            {loading && (
+                              <span
+                                className="spinner-grow spinner-grow-sm"
+                                role="status"
+                                aria-hidden="true"
+                              ></span>
+                            )}
+                            Log In
+                            <Image
+                              className="position-absolute top-50 end-0 me-2 translate-middle"
+                              src="/images/icons/btn_arrow.svg"
+                              alt="btn_arrow"
+                              width={24}
+                              height={25}
+                            />
+                          </button>
+                        </div>
+                        {/*                   <div className="text-center d-flex flex-md-row align-items-center justify-content-center">
                     <div className="col">
                       <hr />
                     </div>
@@ -295,17 +300,20 @@ export default function LoginForm() {
                       height={48}
                     />
                   </div> */}
-                  <div className="col-md-12 text-center d-flex flex-column flex-md-row align-items-center justify-content-center">
-                    <span>Don't have an account?</span>
-                    <button
-                      className="btn btn-link px-1 py-0 text-decoration-none"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        router.push("/signup");
-                      }}
-                    >
-                      Sign up
-                    </button>
+                        <div className="col-md-12 text-center d-flex flex-column flex-md-row align-items-center justify-content-center">
+                          <span>Don't have an account?</span>
+                          <button
+                            className="btn btn-link px-1 py-0 text-decoration-none"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              router.push("/signup");
+                            }}
+                          >
+                            Sign up
+                          </button>
+                        </div>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
