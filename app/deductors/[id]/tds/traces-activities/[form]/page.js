8fd8d79b90911e2ea4deae1fe3b0b0aaddaeec44
Developer123485 <Developer123485@example.com>
2025-09-09 09:39:06 +0530

    bug fixed

--- app/deductors/[id]/tds/traces-activities/[form]/page.js
+++ app/deductors/[id]/tds/traces-activities/[form]/page.js
@@ -323,6 +323,7 @@ export default function TracesActivities({ params }) {
   }
 
   function submitLogin(e, value = false) {
+    e.preventDefault();
     if (validateTraces() || value) {
       setSubmitLoading(true);
       if (tracesActivity.userName && tracesActivity.password && deductorInfo?.deductorTan) {
