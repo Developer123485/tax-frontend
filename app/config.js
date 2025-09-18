24a5d31dfb3a7a8e8712843b4c1a83a2ba9a49eb
Developer123485 <Developer123485@example.com>
2025-09-18 19:32:45 +0530

    bug fixed

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const baseUrl = "https://api.taxvahan.site/api/";
+const baseUrl = "http://139.84.175.29:5000/api/";
 export const apiRequest = axios.create({ baseURL: baseUrl });
-export const apiUrl = "https://api.taxvahan.site/api/";
+export const apiUrl = "http://139.84.175.29:5000/api/";
