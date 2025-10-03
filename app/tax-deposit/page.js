c36555e826d9f2c0ed79b7c50e3a17cffc293a0c
Developer123485 <Developer123485@example.com>
2025-10-03 13:02:55 +0530

    bug fixed

--- app/tax-deposit/page.js
+++ app/tax-deposit/page.js
@@ -352,6 +352,7 @@ export default function TaxDeposit() {
       pageSize: pageSize,
       pageNumber: currentPage,
       search: searchValue,
+      financialYear: financialYear
     };
     if (
       searchParams.get("token") ==
