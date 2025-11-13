d5d0aee7349b2050f565259d604d98c9bcead958
Developer123485 <Developer123485@example.com>
2025-11-13 18:00:31 +0530

    api changes

--- app/services/common.service.js
+++ app/services/common.service.js
@@ -13,7 +13,7 @@ export const CommonService = {
   formatDate
 };
 
-function formatDate(params) {
+function formatDate(dateString) {
   const date = new Date(dateString);
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
