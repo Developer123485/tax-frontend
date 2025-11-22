d3081d861258fa32ab67957cfcf25a2bf9bea725
Developer123485 <Developer123485@example.com>
2025-11-22 22:36:03 +0530

    text changes

--- app/components/deductors/searchable-dropdown.js
+++ app/components/deductors/searchable-dropdown.js
@@ -15,23 +15,44 @@ export default function SearchableDropdown(props) {
 
     return (
         <Dropdown show={showDropdown} onToggle={() => setShowDropdown(!showDropdown)} onSelect={handleSelect}>
-            <Dropdown.Toggle className='bg-white text-dark broder border-secondary border-1 w-100 text-start' style={{ minWidth: '450px' }}>
-                {filteredOptions && filteredOptions.length > 0 && props.id && filteredOptions.find(p => p.key == props.id)?.value ? filteredOptions.find(p => p.key == props.id)?.value : "Select"}
-                {props.id && <span className='ms-2' onClick={(e) => props.setEventId("")}><i className="fa-solid fa-circle-xmark"></i></span>}
+            <Dropdown.Toggle
+                className='bg-white text-dark border border-secondary w-100 text-start'
+                style={{ minWidth: '450px' }}
+            >
+                {filteredOptions && filteredOptions.length > 0 && props.id &&
+                    filteredOptions.find(p => p.key == props.id)?.value
+                    ? filteredOptions.find(p => p.key == props.id)?.value
+                    : "Select"
+                }
+
+                {props.id && (
+                    <span className='ms-2' onClick={(e) => { e.stopPropagation(); props.setEventId(""); }}>
+                        <i className="fa-solid fa-circle-xmark"></i>
+                    </span>
+                )}
             </Dropdown.Toggle>
-            <Dropdown.Menu className='ps-2 pe-3 py-2 w-100' style={{ minWidth: '450px', maxHeight: '190px', overflowX: 'hidden', overflowY: 'scroll' }}>
+
+            <Dropdown.Menu
+                className='ps-2 pe-3 py-2 w-100'
+                style={{ minWidth: '450px', maxHeight: '190px', overflowX: 'hidden', overflowY: 'scroll' }}
+            >
                 <Form.Control
                     autoFocus
                     type="text"
                     placeholder="Search..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
-                    onClick={(e) => e.stopPropagation()} // Prevent menu from closing on click
+                    onClick={(e) => e.stopPropagation()}
                     style={{ margin: '5px' }}
                 />
+
                 {filteredOptions.length > 0 ? (
                     filteredOptions.map((option, index) => (
-                        <Dropdown.Item eventKey={option.key} key={index}>
+                        <Dropdown.Item
+                            eventKey={option.key}
+                            key={index}
+                            active={props.id === option.key}   // <-- highlight selected item
+                        >
                             {option.value}
                         </Dropdown.Item>
                     ))
@@ -39,6 +60,6 @@ export default function SearchableDropdown(props) {
                     <Dropdown.Item disabled>No results found</Dropdown.Item>
                 )}
             </Dropdown.Menu>
-        </Dropdown >
+        </Dropdown>
     );
 };
