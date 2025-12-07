3f003105ad02d3f9400b83a2837f9a179c39d397
Developer123485 <Developer123485@example.com>
2025-12-07 13:20:57 +0530

    bug fixed

--- app/components/deductors/deductor-detail/page.js
+++ app/components/deductors/deductor-detail/page.js
@@ -10,7 +10,6 @@ export default function DeductorDetail(props) {
   const [isFocused1, setIsFocused1] = useState(false);
   const [isFocused2, setIsFocused2] = useState(false);
   const [isFocused3, setIsFocused3] = useState(false);
-  const [isFocused4, setIsFocused4] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showTracesPassword, setShowTracesPassword] = useState(false);
   const highlightStyle = {
@@ -44,13 +43,6 @@ export default function DeductorDetail(props) {
     boxShadow: isFocused3 ? "0 0 3px 2px rgba(0, 123, 255, 0.5)" : "none",
     outline: "none",
   };
-  const highlightStyle4 = {
-    padding: "8px",
-    border: "1px solid",
-    borderColor: isFocused3 ? "#007bff" : "#ccc",
-    boxShadow: isFocused3 ? "0 0 3px 2px rgba(0, 123, 255, 0.5)" : "none",
-    outline: "none",
-  };
 
 
   const router = useRouter(null);
@@ -71,53 +63,132 @@ export default function DeductorDetail(props) {
       {enumList && (
         <form autoComplete="off">
           <div>
-            {!props.isCorrection && <div className="row bg-light-gray px-3 py-4 px-md-2 py-md-3 rounded-3 mb-5 g-3">
+            <div className="row bg-light-gray px-3 py-4 px-md-2 py-md-3 rounded-3 mb-5 g-3">
               <div className="row mb-2">
-                <div className="col-md-6 d-flex align-items-center">
-                  <h5 className="text-blue fw-bold mb-0">
-                    ITD Login{" "}
-                  </h5>
-                  {deductorDetail.isITDStatus && deductorDetail.itdLogin && deductorDetail.itdPassword && <span className="badge bg-success ms-2 mt-0" style={{ fontSize: "0.85rem" }}>
-                    Verified
-                  </span>
-                  }
+                <div className="col-md-6">
+                  <h5 className="text-blue fw-bold">ITD Login</h5>
                 </div>
-                <div className="col-md-6 d-flex align-items-center">
-                  <h5 className="text-blue fw-bold mb-0">
-                    Traces Login{" "}
-                  </h5>
-                  {deductorDetail.isTracesStatus && deductorDetail.tracesLogin && deductorDetail.tracesPassword && <span className="badge bg-success ms-2 mt-0" style={{ fontSize: "0.85rem" }}>
-                    Verified
-                  </span>
-                  }
+                <div className="col-md-6">
+                  <h5 className="text-blue fw-bold">Traces Login</h5>
                 </div>
               </div>
-            </div>}
+              <div className="row g-3 mt-0 d-flex align-items-end">
+                <div className="col-md-2">
+                  <label htmlFor="inputTAN" className="form-label">
+                    <span>TAN</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="ABCD10787A"
+                    className="form-control"
+                    id="inputTAN"
+                    maxLength={20}
+                    autoComplete="off"
+                    value={deductorDetail.itdLogin}
+                    onChange={(e) => {
+                      e.target.value = e.target.value.toUpperCase();
+                      handleInput("itdLogin", e);
+                    }}
+                  />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="inputPassword" className="form-label">
+                    <span>Password</span>
+                  </label>
+                  <div className="input-group ">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      className="form-control"
+                      id="inputPassword"
+                      name="no-autofill-password"
+                      autoComplete="new-password"
+                      maxLength={25}
+                      value={deductorDetail.itdPassword ? deductorDetail.itdPassword : ""}
+                      onChange={(e) => handleInput("itdPassword", e)}
+                    />
+                    <button type="button" className="eye-icon"
+                      onClick={() => setShowPassword((prev) => !prev)}
+                    >
+                      {showPassword ? <FaEye /> : <FaEyeSlash />}
+                    </button>
+                  </div>
+                </div>
+                <div className="col-md-1 justify-content-end">
+                  <button type="button" className="btn btn-primary px-2 w-100">
+                    Auto Fill
+                  </button>
+                </div>
+                <div className="col-md-2">
+                  <label htmlFor="inputUserName" className="form-label">
+                    <span>User Name</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder="User Name"
+                    className="form-control"
+                    id="inputUserName"
+                    name="no-autofill-username"
+                    maxLength={20}
+                    autoComplete="off"
+                    value={deductorDetail.tracesLogin}
+                    onChange={(e) => handleInput("tracesLogin", e)}
+                  />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="inputUserPassword" className="form-label">
+                    <span>Password</span>
+                  </label>
+                  <div className="input-group ">
+                    <input
+                      type={showTracesPassword ? "text" : "password"}
+                      placeholder="Password"
+                      className="form-control"
+                      id="inputUserPassword"
+                      maxLength={20}
+                      autoComplete="new-password"
+                      value={deductorDetail.tracesPassword}
+                      onChange={(e) => handleInput("tracesPassword", e)}
+                    />
+                    <button type="button" className="eye-icon"
+                      onClick={() => setShowTracesPassword((prev) => !prev)}
+                    >
+                      {showTracesPassword ? <FaEye /> : <FaEyeSlash />}
+                    </button>
+                  </div>
+                </div>
+                <div className="col-md-1 justify-content-end">
+                  <button type="button" className="btn btn-primary" onClick={(e) => props.verify(e)}>
+                    Verify
+                  </button>
+                </div>
+              </div>
+            </div>
             <div className="row bg-light-gray px-3 py-4 px-md-2 py-md-3 rounded-3 mb-4 g-3">
               <div className="col-md-12 mt-0">
                 <h5 className="text-blue fw-bold">Deductor Details</h5>
               </div>
-              {props.type === "regular" &&
-                <div className="col-md-3">
-                  <label htmlFor="inputCodeNo" className="form-label">
-                    <span>Code No</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder=""
-                    className="form-control"
-                    id="inputCodeNo"
-                    maxLength={10}
-                    autoComplete="off"
-                    value={deductorDetail.deductorCodeNo}
-                    onChange={(e) => {
-                      if (e.target.value) {
-                        e.target.value = e.target.value.trim();
-                      }
-                      handleInput("deductorCodeNo", e)
-                    }}
-                  />
-                </div>}
+              <div className="col-md-3">
+                <label htmlFor="inputCodeNo" className="form-label">
+                  <span>Code No</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder=""
+                  className="form-control"
+                  id="inputCodeNo"
+                  maxLength={10}
+                  autoComplete="off"
+                  value={deductorDetail.deductorCodeNo}
+                  onChange={(e) => {
+                    if (e.target.value) {
+                      e.target.value = e.target.value.trim();
+                    }
+                    handleInput("deductorCodeNo", e)
+                  }}
+                />
+              </div>
               <div className="col-md-3">
                 <label htmlFor="inputName" className="form-label">
                   <span>Deductor Name</span>
@@ -180,7 +251,6 @@ export default function DeductorDetail(props) {
                   className="form-control"
                   id="inputTANNo"
                   autoComplete="off"
-                  disabled={props.isCorrection}
                   maxLength={10}
                   value={
                     deductorDetail.deductorTan
@@ -560,6 +630,212 @@ export default function DeductorDetail(props) {
                 )}
               </div>
             </div>
+            <div className="row my-4 my-md-4">
+              <div className="col-md-12 mb-3">
+                <h5 className="fw-bold">Type of Deductor</h5>
+              </div>
+              <div className="col-md-12 mb-3">
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="RadioGovernment"
+                    id="inlineRadio1"
+                    value="government"
+                    checked={deductorDetail.governmentType === "government"}
+                    onChange={(e) => handleInput("governmentType", e)}
+                  />
+                  <label className="form-check-label" htmlFor="RadioGovernment">
+                    Government
+                  </label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name="RadioOther"
+                    id="inlineRadio2"
+                    value="other"
+                    checked={deductorDetail.governmentType === "other"}
+                    onChange={(e) => handleInput("governmentType", e)}
+                  />
+                  <label className="form-check-label" htmlFor="RadioOther">
+                    Other
+                  </label>
+                </div>
+              </div>
+            </div>
+            {deductorDetail.governmentType === "government" && (
+              <div className="row bg-light-gray px-3 py-4 px-md-2 py-md-3 rounded-3 mb-4 g-3">
+                <div className="col-md-12 mt-0">
+                  <h5 className="text-blue fw-bold">Government Details</h5>
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="inputDDOCode" className="form-label">
+                    <span>DDO Code</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="inputDDOCode"
+                    autoComplete="off"
+                    maxLength={15}
+                    value={deductorDetail.ddoCode}
+                    onChange={(e) => handleInput("ddoCode", e)}
+                  />
+                </div>
+
+                <div className="col-md-3">
+                  <label htmlFor="inputDDORegistrationNo" className="form-label">
+                    <span>DDO Registration No.</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="inputDDORegistrationNo"
+                    autoComplete="off"
+                    maxLength={15}
+                    value={deductorDetail.ddoRegistration}
+                    onChange={(e) => handleInput("ddoRegistration", e)}
+                  />
+                </div>
+
+                <div className="col-md-3">
+                  <label htmlFor="inputMinistryName" className="form-label">
+                    <span>Ministry Name</span>
+                  </label>
+                  <select
+                    className="form-select"
+                    aria-label="Default select example"
+                    autoComplete="off"
+                    maxLength={15}
+                    style={highlightStyle2}
+                    onFocus={() => setIsFocused2(true)}
+                    onBlur={() => setIsFocused2(false)}
+                    value={deductorDetail.ministryName}
+                    onChange={(e) => handleInput("ministryName", e)}
+                  >
+                    <option value={""}>Select Ministry</option>
+                    {enumList.ministries?.map((option, index) => (
+                      <option key={index} value={option.key}>
+                        {option.value}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                {(deductorDetail.deductorType == "S" || deductorDetail.deductorType == "E" || deductorDetail.deductorType == "H" || deductorDetail.deductorType == "N") &&
+                  <div className="col-md-3">
+                    <label htmlFor="inputMinistryState" className="form-label">
+                      <span>Ministry State</span>
+                    </label>
+
+                    <select
+                      className="form-select"
+                      aria-label="Default select example"
+                      autoComplete="off"
+                      maxLength={15}
+                      style={highlightStyle3}
+                      onFocus={() => setIsFocused3(true)}
+                      onBlur={() => setIsFocused3(false)}
+                      value={deductorDetail.ministryState}
+                      onChange={(e) => handleInput("ministryState", e)}
+                    >
+                      <option value={""}>Select State</option>
+                      {enumList.states?.filter(p => p.value != "OVERSEAS")?.map((option, index) => (
+                        <option key={index} value={option.key}>
+                          {option.value}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                }
+                <div className="col-md-3">
+                  <label htmlFor="inputPADCode" className="form-label">
+                    <span>PAO Code</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="inputPADCode"
+                    autoComplete="off"
+                    maxLength={15}
+                    value={deductorDetail.paoCode}
+                    onChange={(e) => handleInput("paoCode", e)}
+                  />
+                </div>
+
+                <div className="col-md-3">
+                  <label htmlFor="inputAINCode" className="form-label">
+                    <span>AIN Code</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="inputAINCode"
+                    autoComplete="off"
+                    maxLength={15}
+                    value={deductorDetail.ainCode}
+                    onChange={(e) => handleInput("ainCode", e)}
+                  />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="inputPAORegNo" className="form-label">
+                    <span>PAO Reg No.</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="inputPAORegNo"
+                    autoComplete="off"
+                    maxLength={20}
+                    value={deductorDetail.paoRegistration}
+                    onChange={(e) => handleInput("paoRegistration", e)}
+                  />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="inputMinistryNameOther" className="form-label">
+                    <span>Other Ministry</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="inputMinistryNameOther"
+                    autoComplete="off"
+                    maxLength={20}
+                    value={deductorDetail.ministryNameOther}
+                    onChange={(e) => handleInput("ministryNameOther", e)}
+                  />
+                </div>
+                <div className="col-md-3">
+                  <label htmlFor="officeIN" className="form-label">
+                    <span>Office Identification Number</span>
+                  </label>
+
+                  <input
+                    type="text"
+                    placeholder=""
+                    className="form-control"
+                    id="officeIN"
+                    autoComplete="off"
+                    maxLength={20}
+                    value={deductorDetail.identificationNumber}
+                    onChange={(e) => handleInput("identificationNumber", e)}
+                  />
+                </div>
+              </div>
+            )}
             <div className="row">
               <div className="col-md-12 px-0 d-flex justify-content-start">
                 {deductorDetail.id > 0 && (
