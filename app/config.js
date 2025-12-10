67a63d04cb8390211844179f6716af405b873e6e
Developer123485 <Developer123485@example.com>
2025-12-10 21:37:19 +0530

    revert chages

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const baseUrl = "http://localhost:5008/api/";
+const baseUrl = "https://api.taxvahan.site/api/";
 export const apiRequest = axios.create({ baseURL: baseUrl });
-export const apiUrl = "http://localhost:5008/api/";
+export const apiUrl = "https://api.taxvahan.site/api/";
