2e14a904b11336ee08f5eb68556a2811010d4335
Developer123485 <Developer123485@example.com>
2025-12-27 13:34:28 +0530

    bug fixed

--- app/components/banner/home-banner.js
+++ app/components/banner/home-banner.js
@@ -1,18 +1,23 @@
 "use client";
-import React from "react";
 import Image from "next/image";
 
 const HomeBanner = ({ subheading, heading, paragraph, backgroundImage }) => {
   return (
-    <div
-      className="col-md-6 d-flex justify-content-end align-items-center main-banner"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="px-2 py-5 mx-md-4 px-md-5 py-md-5 text-white">
+    <div className="col-md-6 position-relative d-flex align-items-center justify-content-end main-banner overflow-hidden">
+
+      {/* Background Image */}
+      <Image
+        src={backgroundImage}
+        alt="Banner Background"
+        fill
+        priority
+        quality={80}
+        className="object-cover"
+        sizes="(max-width: 768px) 100vw, 50vw"
+      />
+
+      {/* Overlay Content */}
+      <div className="position-relative text-white px-2 py-5 mx-md-4 px-md-5 py-md-5 z-1">
         <h6 className="fw-bold mb-2">{subheading}</h6>
         <h1 className="fw-bold mb-3">{heading}</h1>
         <p className="fs-18">{paragraph}</p>
